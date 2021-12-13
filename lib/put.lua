0312ffa5e304e7a582f464cbb0a0da9299b414d9
Joshua T. Casey <joshua.t.casey@auth0.com>
2021-12-13 10:50:53 -0600

    Refactor put.lua for readability

--- lib/put.lua
+++ lib/put.lua
@@ -6,20 +6,19 @@ local current_time = redis.call('TIME')
 local current_timestamp_ms = current_time[1] * 1000 + current_time[2] / 1000
 
 local current_remaining = redis.call('HMGET', KEYS[1], 'r')[1]
-if current_remaining == false then
-  current_remaining = bucket_size
-end
+local new_remaining
 
-local new_content = math.min(current_remaining + tokens_to_add, bucket_size)
+new_remaining = tokens_to_add + (current_remaining and current_remaining or bucket_size)
+new_remaining = math.min(new_remaining, bucket_size)
 
 redis.replicate_commands()
-if new_content < bucket_size then
+if new_remaining < bucket_size then
   redis.call('HMSET', KEYS[1],
             'd', current_timestamp_ms,
-            'r', new_content)
+            'r', new_remaining)
   redis.call('EXPIRE', KEYS[1], ttl)
 else
   redis.call('DEL', KEYS[1])
 end
 
-return { new_content, current_timestamp_ms }
+return { new_remaining, current_timestamp_ms }

f52e845595df84ce34ebcc64df013dac124e21a7
Joshua T. Casey <joshua.t.casey@auth0.com>
2022-01-11 16:02:27 -0600

    Change variable names in take.lua

--- lib/take.lua
+++ lib/take.lua
@@ -1,6 +1,6 @@
 local tokens_per_ms        = tonumber(ARGV[1])
 local bucket_size          = tonumber(ARGV[2])
-local new_content          = tonumber(ARGV[2])
+local new_remaining        = tonumber(ARGV[2])
 local tokens_to_take       = tonumber(ARGV[3])
 local ttl                  = tonumber(ARGV[4])
 
@@ -16,19 +16,19 @@ end
 if current[1] and tokens_per_ms then
     -- drip bucket
     local last_drip = current[1]
-    local content = current[2]
+    local current_remaining = current[2]
     local delta_ms = math.max(current_timestamp_ms - last_drip, 0)
     local drip_amount = delta_ms * tokens_per_ms
-    new_content = math.min(content + drip_amount, bucket_size)
+    new_remaining = math.min(current_remaining + drip_amount, bucket_size)
 elseif current[1] and tokens_per_ms == 0 then
     -- fixed bucket
-    new_content = current[2]
+    new_remaining = current[2]
 end
 
-local enough_tokens = new_content >= tokens_to_take
+local enough_tokens = new_remaining >= tokens_to_take
 
 if enough_tokens then
-    new_content = math.min(new_content - tokens_to_take, bucket_size)
+    new_remaining = math.min(new_remaining - tokens_to_take, bucket_size)
 end
 
 -- https://redis.io/commands/EVAL#replicating-commands-instead-of-scripts
@@ -36,7 +36,7 @@ redis.replicate_commands()
 
 redis.call('HMSET', KEYS[1],
             'd', current_timestamp_ms,
-            'r', new_content)
+            'r', new_remaining)
 redis.call('EXPIRE', KEYS[1], ttl)
 
-return { new_content, enough_tokens, current_timestamp_ms }
+return { new_remaining, enough_tokens, current_timestamp_ms }
